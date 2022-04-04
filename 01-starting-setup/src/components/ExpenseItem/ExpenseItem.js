1a919201caff8d9ca5ea989d53db331bf95462da
Marina Cerame <marina.cerame@gmail.com>
2022-04-04 10:04:34 -0500

    [36] Add variable interpolation to ExpenseItem component

--- 01-starting-setup/src/components/ExpenseItem/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 3, 4);
+  const expenseTitle = 'Udemy Course';
+  const expenseAmount = 20.0;
+
   return (
     <div className="expense-item">
-      <div>April 4, 2022</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Udemy course</h2>
-        <div className="expense-item__price">$20.00</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
