4da5320acd2b8922184e5568c140e1091019297e
Marina Cerame <marina.cerame@gmail.com>
2022-04-04 10:20:11 -0500

    [37] Make ExpenseItem component reusable

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,30 @@
-import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Udemy course', amount: '20.00', date: new Date(2022, 3, 4) },
+    { title: 'Dice', amount: '12.59', date: new Date(2022, 3, 4) },
+    { title: 'Dice (fancy)', amount: '76.82', date: new Date(2022, 3, 4) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseItem/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 3, 4);
-  const expenseTitle = 'Udemy Course';
-  const expenseAmount = 20.0;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
