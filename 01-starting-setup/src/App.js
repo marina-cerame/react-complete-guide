c9b2f0958a2329f06da8370c8c7c7beab4831a4b
Marina Cerame <marina.cerame@gmail.com>
2022-04-04 09:38:56 -0500

    [32] Initial commit

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible! Although it's very hard to see, idk why they picked a dark gray background.</p>
+    </div>
+  );
+}
+
+export default App;
