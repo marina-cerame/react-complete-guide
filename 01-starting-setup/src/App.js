ffb5e2ba9ce0993528364b34571dc21d071a41ab
Marina Cerame <marina.cerame@gmail.com>
2022-04-04 22:21:16 -0500

    [38] Update date handling in ExpenseItem component
    * Change dates in App component to better show date handling

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -3,8 +3,8 @@ import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 function App() {
   const expenses = [
     { title: 'Udemy course', amount: '20.00', date: new Date(2022, 3, 4) },
-    { title: 'Dice', amount: '12.59', date: new Date(2022, 3, 4) },
-    { title: 'Dice (fancy)', amount: '76.82', date: new Date(2022, 3, 4) },
+    { title: 'Dice', amount: '12.59', date: new Date(2022, 2, 12) },
+    { title: 'Dice (fancy)', amount: '76.82', date: new Date(2022, 1, 28) },
   ];
 
   return (
