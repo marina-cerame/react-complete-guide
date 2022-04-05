4b749d29dcf4ffa118cb878251bdfafb5b1778db
Marina Cerame <marina.cerame@gmail.com>
2022-04-04 23:18:34 -0500

    [40] Add Card component
    Adds Card component as a wrapper component, applying
    rounded corner and drop shadow styles

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
+import Card from '../Card/Card';
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
 import './Expenses.css';
 
 function Expenses({ items }) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {items.map((e) => (
         <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
       ))}
-    </div>
+    </Card>
   );
 }
 
