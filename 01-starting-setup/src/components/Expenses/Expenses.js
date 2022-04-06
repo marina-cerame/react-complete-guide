c0f30199a8fe5216fc080737e9616236e9edc58a
Marina Cerame <marina.cerame@gmail.com>
2022-04-06 10:34:20 -0500

    [A2] Add ExpensesFilter component
    Adds dropdown which will be used to filter expenses by
    year. Currently only passes the selected year up to
    Expenses component.

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+
 import './Expenses.css';
 
 const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+  const onFilterSelectHandler = (filter) => {
+    console.log('Filtered!', filter);
+    setFilteredYear(filter);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterSelect={onFilterSelectHandler}
+      />
+
       {items.map((e) => (
         <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
       ))}
