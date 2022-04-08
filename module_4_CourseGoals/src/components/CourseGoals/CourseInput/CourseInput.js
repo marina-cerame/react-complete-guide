fca7c09996a5a6bb38ccd2e8d0b21ff2fa3a48bb
Marina Cerame <marina.cerame@gmail.com>
2022-04-08 11:43:32 -0500

    [81] Refactor goal input to use css modules, clean up Button
    * Use css module styling for form control
    * Clean out commented code from Button
    * Restore responsive button width

--- module_4_CourseGoals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ module_4_CourseGoals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,34 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+
+import styles from './CourseInput.module.css';
 
 import Button from '../../UI/Button/Button';
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ isValid }) => (isValid ? 'black' : 'red')};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ isValid }) => (isValid ? '#ccc' : 'red')};
-    background: ${({ isValid }) => (isValid ? 'transparent' : 'lightcoral')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -52,10 +26,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl isValid={isValid}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
