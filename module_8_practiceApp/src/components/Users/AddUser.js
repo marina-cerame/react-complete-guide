916f653d16d47e65c23df90f5d649171f91c4959
Marina Cerame <marina.cerame@gmail.com>
2022-04-13 10:43:12 -0500

    [93] Add basic validation, reset form on submit

--- module_8_practiceApp/src/components/Users/AddUser.js
+++ module_8_practiceApp/src/components/Users/AddUser.js
@@ -7,26 +7,64 @@ import styles from './AddUser.module.css';
 const AddUser = () => {
   const [username, setUsername] = useState('');
   const [age, setAge] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const usernameChangeHandler = (e) => {
+    // Reset isValid when user changes input
+    if (!isValid) {
+      setIsValid(true);
+    }
     setUsername(e.target.value);
   };
 
   const ageChangeHandler = (e) => {
+    // Reset isValid when user changes input
+    if (!isValid) {
+      setIsValid(true);
+    }
     setAge(e.target.value);
   };
 
   const submitUserHandler = (e) => {
     e.preventDefault();
+    // Validate values
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    if (age < 1) {
+      setIsValid(false);
+      return;
+    }
+
     console.log(username, age);
+
+    // Reset form
+    setUsername('');
+    setAge('');
   };
+
+  let validationMessage = '';
+
+  if (!isValid) {
+    validationMessage = (
+      <p className={styles.invalid}>Please check your input.</p>
+    );
+  }
+
   return (
-    <Card className={styles.input}>
+    <Card className={`${styles.input} ${!isValid && styles.invalid}`}>
+      {validationMessage}
       <form onSubmit={submitUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" value={age} onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
