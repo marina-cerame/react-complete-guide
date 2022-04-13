73417186dd2e1a707bb61e7d7f382ff43cc43862
Marina Cerame <marina.cerame@gmail.com>
2022-04-13 14:58:49 -0500

    [105] Use portal to render modal at top of body

--- module_9_fragmentsPortalsRefs/src/components/UI/ErrorModal.js
+++ module_9_fragmentsPortalsRefs/src/components/UI/ErrorModal.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
-const ErrorModal = (props) => {
+const Backdrop = ({ onConfirm }) => {
+  return <div className={classes.backdrop} onClick={onConfirm} />;
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = ({ title, message, onConfirm }) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById('modal-root')
+      )}
+    </>
   );
 };
 
